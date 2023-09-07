5ca8c26df56ef2195ba8fb15e94d7337011333fd
Vaxod <110937853+Vaxood@users.noreply.github.com>
2023-09-07 20:09:09 +0200

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -3,32 +3,41 @@
 #include <MinHook.h>
 #include <iostream>
 #include <GarrysMod/Lua/Helpers.hpp>
-
 typedef void (*ConMsg_t)(PRINTF_FORMAT_STRING const tchar* pMsg, ...);
 ConMsg_t original_ConMsg = nullptr;
 GarrysMod::Lua::ILuaBase* gLua = nullptr;
-void hk_ConMsg(PRINTF_FORMAT_STRING const tchar* pMsg, ...)
-{
 
-    va_list args;
-    va_start(args, pMsg);
-    bool call = true;
-   
-    LuaHelpers::PushHookRun(gLua, "OnConMsgCalled");
-    gLua->PushString(pMsg);
-    if (LuaHelpers::CallHookRun(gLua, 1, 1))
-        call = !gLua->IsType(-1, GarrysMod::Lua::Type::BOOL) || gLua->GetBool(-1);
-     gLua->Pop(gLua->Top());
-    if (call)
-    original_ConMsg(pMsg, args);
-    va_end(args);
+std::string GetFormatWithList(const char* fmt, va_list ap) {
+    std::string message;
+    va_list ap_copy;
+    va_copy(ap_copy, ap);
+    size_t len = vsnprintf(0, 0, fmt, ap_copy);
+    message.resize(len + 1);  
+    vsnprintf(&message[0], len + 1, fmt, ap);
+    message.resize(len);  
+    return message;
 }
 
+void hk_ConMsg(const char* format, ...) {
 
+    va_list va;
+	va_start(va, format);
+    std::string message = GetFormatWithList(format, va);
+    va_end(va);
+    bool call = true;
+    LuaHelpers::PushHookRun(gLua, "OnConMsgCalled");
+    gLua->PushString(message.c_str());
+    if (LuaHelpers::CallHookRun(gLua, 1, 1))
+        call = !gLua->IsType(-1, GarrysMod::Lua::Type::BOOL) || gLua->GetBool(-1);
+    gLua->Pop(gLua->Top());
+    if (call)
+        original_ConMsg(message.c_str());
+}
 
 void Init()
 {
     MH_Initialize();
+
     void (*pConMsg)(const char* format, ...) = &ConMsg;
     MH_CreateHook(reinterpret_cast<void*>(pConMsg), reinterpret_cast<void*>(&hk_ConMsg), reinterpret_cast<void**>(&original_ConMsg));
     MH_EnableHook(MH_ALL_HOOKS);
